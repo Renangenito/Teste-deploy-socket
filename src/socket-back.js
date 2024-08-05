f0da105367e6836c95bbea681fa2e9e92e4ea04a
renan lima <renangenito@hotmail.com>
2024-08-05 09:30:27 -0300

    Subindo o projeto para testar na Vercel 6

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,22 +1,10 @@
 import "dotenv/config";
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-
 import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosLogin from "./registrarEventos/login.js";
+import io from "./servidor.js";
 import autorizarAcesso from "./middlewares/autorizarAcesso.js";
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-// Endpoint HTTP básico
-app.get('/', (req, res) => {
-  res.send('WebSocket server is running.');
-});
 
 const nspUsuarios = io.of("/usuarios");
 
@@ -30,9 +18,4 @@ nspUsuarios.on("connection", (socket) => {
 io.of("/").on("connection", (socket) => {
     registrarEventosCadastro(socket, io);
     registrarEventosLogin(socket, io);
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
 });
